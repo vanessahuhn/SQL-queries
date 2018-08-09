fd9c528ca83a824ee4ea95ab6fd7145439f4c2b0
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-09 14:38:07 +0200

    select

---
+++ exercice_people/select.sql
@@ -0,0 +1,7 @@
+-- Donner le nom et le prénom de chaque personne qui existe dans la table passager
+SELECT nom, prenom
+FROM passager;
+
+-- Donner toutes les informations pour la table personne
+SELECT *
+FROM personne;
