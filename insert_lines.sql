b79674ff1ff84d53f679f005daa97d45a11cb40d
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-09 11:13:18 +0200

    inserer lignes

---
+++ insert_lines.sql
@@ -0,0 +1,18 @@
+-- Comment ajouter deux lignes d’information hotels dans votre table hotel ? 
+INSERT INTO `projet_hotel_sql`.`hotel`
+(`id`,
+`nom`,
+`nb_etoile`)
+VALUES
+(<{id: 1}>,
+<{nom: 'Hotel Jonasz'}>,
+<{nb_etoile: 4}>);
+
+INSERT INTO `projet_hotel_sql`.`hotel`
+(`id`,
+`nom`,
+`nb_etoile`)
+VALUES
+(<{id: 2}>,
+<{nom: 'Hotel Constance'}>,
+<{nb_etoile: 5}>);
