437a2f4ce62eb114a84929b9061b721513536fd6
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-10 12:06:10 +0200

    création de la table SALLE_DE_RESTAURATION + auto-incrémentation

--- exercice_restaurants/create_table.sql
+++ exercice_restaurants/create_table.sql
@@ -2,8 +2,8 @@
 USE bd_restaurant;
 -- créer une table SALLE_DE_RESTAURATION qui contient
 CREATE TABLE SALLE_DE_RESTAURATION (
-	-- le champ idRestauration qui aura des valeurs de type int
-	idRestauration INT,
+	-- le champ idRestauration qui aura des valeurs de type int, ne pourra pas être NULL et s'auto-incrémente
+	idRestauration INT NOT NULL AUTO_INCREMENT,
     -- le champ nombreTables qui aura des valeurs de type int
     nombreTables INT,
     -- le champ nombrePlaces qui aura des valeurs de type int
