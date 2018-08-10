7f9db31a35555f633a1b75f21f5cc28619a14bf5
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-10 12:00:18 +0200

    création de la table SALLE_DE_RESTAURATION

---
+++ exercice_restaurants/create_table.sql
@@ -0,0 +1,13 @@
+-- Dans la base de données bd_restaurant
+USE bd_restaurant;
+-- créer une table SALLE_DE_RESTAURATION qui contient
+CREATE TABLE SALLE_DE_RESTAURATION (
+	-- le champ idRestauration qui aura des valeurs de type int
+	idRestauration INT,
+    -- le champ nombreTables qui aura des valeurs de type int
+    nombreTables INT,
+    -- le champ nombrePlaces qui aura des valeurs de type int
+    nombrePlaces INT,
+    -- La clé primaire de cette table sera le champ idRestauration
+    PRIMARY KEY(idRestauration)
+);
