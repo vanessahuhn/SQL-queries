95822eb0936486d3a40c0d17744b0fdd4a0c87b8
vanessahuhn <vanessa.huhn@gmail.com>
2018-08-10 13:36:15 +0200

    insérer des lignes dans la table restaurant

---
+++ exercice_restaurants/insert_lines_restaurant.sql
@@ -0,0 +1,6 @@
+INSERT INTO `bd_restaurant`.`restaurant`
+(`nomRestaurant`, `typeCuisine`, `nomProprietaire`, `nombreEmployes`, `nombreSalles`)
+VALUES
+('Chez Lolo', 'Japonaise', 'Logan', 1, 4),
+('Chez Poulouin', 'Du Ch\'Nord', 'Pierre', 1, 6),
+('Chez Vaness\'', 'Bretonne', 'Vanessa', 1, 6);
